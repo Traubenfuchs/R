0e80afbd7c6a2344dedb47d9f0c3af29a2cdbae4
Fzum <sellemondfabian@gmail.com>
2019-06-02 13:12:56 +0200

    simpsons

--- 4/app.r
+++ 4/app.r
@@ -6,7 +6,9 @@ ui = pageWithSidebar(
                checkboxInput("classInputId", "class"),
                checkboxInput("sexInputId", "sex"),
                checkboxInput("ageInputId", "age"),
-               checkboxInput("shadeInputId", "shade")
+               checkboxInput("shadeInputId", "shade"),
+               hr(),
+               checkboxInput("simpsonInputId", "simpson paradox")
                ),
   mainPanel(
     width = 10,
@@ -15,7 +17,15 @@ ui = pageWithSidebar(
       fluid = TRUE,
       tabsetPanel(
         tabPanel("Mosaik", plotOutput("mosaikplot"))
-      )
+      ),
+      dataTableOutput("thirdclassAll"),
+      dataTableOutput("crewAll"),
+      hr(),
+      hr(),
+      dataTableOutput("thirdclassMen"),
+      dataTableOutput("thirdclassWomen"),
+      dataTableOutput("crewMen"),
+      dataTableOutput("crewWomen")
     )
   )
 )
@@ -38,15 +48,90 @@ createMosaiqPlot = function(input) {
   formula = as.formula(formulaString)
   mosaicplot = mosaicplot(formula, main = "Survival on the Titanic", 
                           data = Titanic,
-                          pop = FALSE, 
                           shade = input$shadeInputId, 
                           direction = "v",
                           color = TRUE)
   return(mosaicplot)
 }
 
+createThirdclassAll = function(isShowSimpson){
+  titanic = data.frame(Titanic)
+  ThirdClassData = subset(titanic, Class == "3rd")
+  
+  return(createTable(isShowSimpson, ThirdClassData, "Third (Men & Women)"))
+}
+
+createThirdClassMen = function(isShowSimpson){
+  titanic = data.frame(Titanic)
+  ThirdClassData = subset(titanic, Class == "3rd" & Sex == "Male")
+  
+  return(createTable(isShowSimpson, ThirdClassData, "Third (Men)"))
+}
+
+createThirdClassWomen = function(isShowSimpson){
+  titanic = data.frame(Titanic)
+  ThirdClassData = subset(titanic, Class == "3rd" & Sex == "Female")
+  
+  return(createTable(isShowSimpson, ThirdClassData, "Third (Women)"))
+}
+
+createCrewAll = function(isShowSimpson){
+  titanic = data.frame(Titanic)
+  CrewAllData = subset(titanic, Class == "Crew")
+  
+  return(createTable(isShowSimpson, CrewAllData, "Crew (Men & Women)"))
+}
+
+createCrewMen = function(isShowSimpson){
+  titanic = data.frame(Titanic)
+  CrewAllData = subset(titanic, Class == "Crew" & Sex == "Male")
+  
+  return(createTable(isShowSimpson, CrewAllData, "Crew (Men)"))
+}
+
+createCrewWomen = function(isShowSimpson){
+  titanic = data.frame(Titanic)
+  CrewAllData = subset(titanic, Class == "Crew" & Sex == "Female")
+  
+  return(createTable(isShowSimpson, CrewAllData, "Crew (Women)"))
+}
+
+createTable = function(isShowSimpson, dataframe, headertitle){
+  if(isShowSimpson){
+    matrix = matrix(c(headertitle, 
+                      getSumSurvived(dataframe),
+                      getSumLost(dataframe),
+                      getSumSurvived(dataframe) + getSumLost(dataframe),
+                      getSurvivalRate(getSumSurvived(dataframe), (getSumSurvived(dataframe) + getSumLost(dataframe)))
+    ), 
+    ncol = 5, byrow = TRUE)
+    colnames(matrix) <- c("Class","Saved","Lost", "Total", "Survival Rate")
+    
+    return(as.table(matrix))
+  }
+}
+
+getSumSurvived = function(data){
+  return(sum(subset(data, Survived == "Yes")$Freq))
+}
+
+getSumLost = function(data){
+  return(sum(subset(data, Survived == "No")$Freq))
+}
+
+getSurvivalRate = function(survivedSum, total){
+  return(survivedSum / (total / 100))
+}
+
 server = function(input, output) {
   output$mosaikplot = renderPlot(createMosaiqPlot(input))
+  
+  output$thirdclassAll = renderDataTable(createThirdclassAll(input$simpsonInputId))
+  output$crewAll = renderDataTable(createCrewAll(input$simpsonInputId))
+  output$thirdclassMen = renderDataTable(createThirdClassMen(input$simpsonInputId))
+  output$thirdclassWomen = renderDataTable(createThirdClassWomen(input$simpsonInputId))
+  output$crewMen = renderDataTable(createCrewMen(input$simpsonInputId))
+  output$crewWomen = renderDataTable(createCrewWomen(input$simpsonInputId))
 }
 
 shinyApp(ui = ui, server = server)
